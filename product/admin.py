5ccb77c739776cbb79a6eabfb6d239a0a5daf9bd
Flowmikro <cloplop091@gmail.com>
2023-12-21 18:15:50 +0300

    Настойка модели и админки

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ItemModel
+
+
+@admin.register(ItemModel)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
--- product/models.py
+++ product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class ItemModel(models.Model):
+    name = models.CharField('Название', max_length=50, help_text='Название продукта', unique=True)
+    description = models.TextField('Описание', help_text='Описание продукта')
+    price = models.PositiveIntegerField('Цена', help_text='Цена продукта', default=100)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
